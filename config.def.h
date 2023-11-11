91ce419c84a5f4ec06374c8460ebcb8b61e965d1
rustzzdevel <qqrustzz@gmail.com>
2023-11-11 22:38:19 +0300

    update alttags

--- config.def.h
+++ config.def.h
@@ -11,29 +11,35 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus (TTF):size=12" };
+static const char *fonts[]          = {
+	"Terminus (TTF):size=12",
+	"Font Awesome 6 Brands,Font Awesome 6 Brands Regular:size=12",
+	"Font Awesome 6 Free,Font Awesome 6 Free Solid:size=12",
+	"Font Awesome v4 Compatibility,Font Awesome v4 Compatibility Regular:size=12",
+	"Font Awesome 6 Free,Font Awesome 6 Free Regular:size=12",
+};
 static const char dmenufont[]       = "Terminus (TTF):size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#504945";
+static const char col_gray3[]       = "#bdae93";
+static const char col_gray4[]       = "#ebdbb2";
+static const char col_cyan[]        = "#cc241d";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_gray1  },
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
-    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+  [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tagsalt[] = {"", "", "", "","",  "", "", "", "" };
 static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
 
 static const Rule rules[] = {
@@ -41,10 +47,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{},
-/*	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	  { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
+	/* class             instance    title       tags mask     isfloating   monitor */
+	{ "st-256color",     NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
